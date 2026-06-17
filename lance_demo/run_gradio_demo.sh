f0aac6282931ab9de7d4778e5e11053428128b90
Xiake Sun <xiake.sun@amd.com>
2026-06-17 14:22:25 +0000

    Minor fix
    Signed-off-by: Xiake Sun <xiake.sun@amd.com>

--- lance_demo/run_gradio_demo.sh
+++ lance_demo/run_gradio_demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #  --ulysses-degree 4
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=5,6
 python vllm-omni/examples/offline_inference/lance/gradio_demo.py \
-  --model /models/Lance/ 2>&1 | tee lance_gradio_demo.log
+  --model /models/Lance/ --port 32768
